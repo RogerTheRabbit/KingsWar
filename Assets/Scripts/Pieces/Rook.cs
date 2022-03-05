6530a03168abbf387cb6db8f9363f53239f9ef86
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2022-03-05 16:50:00 -0500

    Rook logic completed

---
+++ Assets/Scripts/Pieces/Rook.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Rook : Piece
+{
+    // Start is called before the first frame update
+
+    public override void init(PieceManager pieceManager, bool white)
+    {
+        base.init(pieceManager, white);
+    }
+
+    public override bool hasMove(Cell start, Cell end)
+    {
+        if (end.currentPiece.white == start.currentPiece.white)
+        {
+            return false;
+        }
+
+        bool x = start.mBoardPosition.x - end.mBoardPosition.x == 0;
+        bool y = start.mBoardPosition.y - end.mBoardPosition.y == 0;
+        return (x && !y) || (!x && y);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
