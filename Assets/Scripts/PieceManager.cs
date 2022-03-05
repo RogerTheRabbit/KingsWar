c79abc61a1f4fd47b38e6b419e8bcaa1d0958e48
Ryan Madulka <rtm1822@rit.edu>
2022-03-05 15:23:09 -0500

    saving work

--- Assets/Scripts/PieceManager.cs
+++ Assets/Scripts/PieceManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour
+public class PieceManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Pieces/Piece.cs
+++ Assets/Scripts/Pieces/Piece.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Board : MonoBehaviour
+public abstract class Piece : EventTrigger
+
+    Piece
 {
     // Start is called before the first frame update
     void Start()
