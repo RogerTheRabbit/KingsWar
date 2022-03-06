1b70aca740e3b6514c1f36a066b677d8c27dea4b
Ryan Madulka <rtm1822@rit.edu>
2022-03-06 06:14:38 -0500

    fixed cards

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -12,6 +12,8 @@ public abstract class Card : EventTrigger
     private Cell targetCell;
     public bool white = false;
 
+    public string text;
+
     public virtual void init(TurnManager turnmanager, PieceManager pieceManager,bool white)
     {
 
@@ -20,6 +22,13 @@ public abstract class Card : EventTrigger
         this.turnManager = turnmanager;
         this.pieceManager = pieceManager;
         this.white = white;
+
+        GameObject cardPanel = new GameObject("cardPanel");
+        cardPanel.transform.SetParent(this.transform);
+        cardPanel.transform.localPosition = new Vector3(-1.37343001f, 1.37884998f, 0);
+        Image cardImage = cardPanel.AddComponent<Image>();
+        cardImage.color = Color.red;
+        cardPanel.GetComponent<RectTransform>().sizeDelta = new Vector2(56, 80);
 
         GameObject manaPanel = new GameObject("manaPanel");
         manaPanel.transform.SetParent(this.transform);
@@ -38,6 +47,23 @@ public abstract class Card : EventTrigger
         manaText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
         manaTextBox.GetComponent<RectTransform>().localScale = new Vector3(0.1f, 0.1f, 0.1f);
         manaTextBox.GetComponent<RectTransform>().sizeDelta = new Vector2(150, 150);
+
+        GameObject cardBorder = new GameObject("cardBorder");
+        cardBorder.transform.SetParent(manaPanel.transform);
+        cardBorder.transform.localPosition = new Vector3(14.7553997f, -26.8768997f, 0);
+        Image cardBorderImage = cardBorder.AddComponent<Image>();
+        cardBorderImage.sprite = Resources.Load<Sprite>("toppng.com-board-game-blank-card-template-743x1025");
+        cardBorder.GetComponent<RectTransform>().sizeDelta = new Vector2(60, 85);
+
+        GameObject cardTextBox = new GameObject("cardText");
+        cardTextBox.transform.SetParent(cardPanel.transform);
+        Text cardText = cardTextBox.AddComponent<Text>();
+        cardText.text = manaCost.ToString();
+        cardText.color = Color.white;
+        cardText.fontSize = 120;
+        cardText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        cardTextBox.GetComponent<RectTransform>().localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        cardTextBox.GetComponent<RectTransform>().sizeDelta = new Vector2(150, 150);
 
     }
 
