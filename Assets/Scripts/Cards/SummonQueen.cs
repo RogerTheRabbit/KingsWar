54c3511aee6e6dca27bfa67dbd9d360d331ea908
Alan Read <adr4069@g.rit.edu>
2022-03-06 06:06:29 -0500

    forgot to save

--- Assets/Scripts/Cards/SummonQueen.cs
+++ Assets/Scripts/Cards/SummonQueen.cs
@@ -7,7 +7,6 @@ public class SummonQueen : Summon
 
     public override bool playCard(Cell pieceLocation)
     {
-        //TODO Also check that the cell is not in the back 2 ranks once turnmanager is done
         if (pieceLocation.currentPiece == null && base.canPlayCard(this))
         {
             if (this.turnManager.isWhiteTurn && pieceLocation.mBoardPosition.y > 1) {
